b6694af272949839469ce8e3b2e1dcc005026fb0
catarina2132-alt <catarina2132@gmail.com>
2026-09-25 12:43:44 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCLBXtg-3VwM4YTbD4OcXU_OthhFsnn8ps",
+  authDomain: "painel-de-backlog.firebaseapp.com",
+  projectId: "painel-de-backlog",
+  storageBucket: "painel-de-backlog.firebasestorage.app",
+  messagingSenderId: "863389774469",
+  appId: "1:863389774469:web:0410fdb381d493253e2fec"
+};
